1a8b4d72036d962cad0efcfed3c1614a7aa67957
Fewatts <feralveswatts@gmail.com>
2023-06-27 17:34:36 -0300

    NavBar and footer

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './Navbar.css';
 import cloud from './../../assets/cloud.svg';
 import { Link } from 'react-router-dom';
