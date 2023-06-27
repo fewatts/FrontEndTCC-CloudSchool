9400171d9511582446e06eddbb4308f9806c837f
Fewatts <feralveswatts@gmail.com>
2023-06-27 20:15:38 -0300

    Responsive home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,15 @@
 import './Home.css';
+import home from './../../assets/home.svg';
 
-export function Home(){
-    return(
+export function Home() {
+    return (
         <main>
-
+            <div className='container'>
+                <h1 className='h1home'>Bem-vindo!</h1>
+                <h2>Este é o nosso web site de divulgação de currículos!</h2>
+                <p>Use a barra de navegação para acessar as informações c:</p>
+            </div>
+            <div className='homepic'><img src={home} alt="pic home" className='Homepic' /></div>
         </main>
     );
 }
