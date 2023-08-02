efd9caac20c15fac062514a6c5bda7951049bcab
Fewatts <feralveswatts@gmail.com>
2023-08-02 10:34:57 -0300

    Ajustes finos para mobile

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -4,10 +4,8 @@ import error from './../../assets/logos/404 Error.svg';
 export function Error() {
     return (
         <main className='error'>
-            <div className='error'>
-                <h1 className='h1error'>Ops...</h1>
-                <img src={error} alt="404" className='imgerror'/>
-            </div>
+            <h1 className='h1error'>Ops...</h1>
+            <picture className='imgerror'><img src={error} alt="404" /></picture>
         </main>
     );
 }
